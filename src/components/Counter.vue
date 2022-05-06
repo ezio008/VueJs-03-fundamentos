0a49c7922e4f290df65ccb839da6771ee3828424
Lorenzo Muñoz Berzosa <lorenzoberzosa94@gmail.com>
2022-05-06 15:00:06 +0200

    Add Indecision Component

---
+++ src/components/Counter.vue
@@ -0,0 +1,68 @@
+<template>
+    <h2> {{ title }}</h2>
+    <p> {{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
+    <div>
+        <button @click="increase">+1</button>
+        <button @click="decrease">-1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: 'Counter'
+        },
+        start: {
+            type: Number,
+            default: 5,
+            validator(value) {
+                return value > 0;
+            }
+        }        
+    },
+    // name:'Counter'
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        // getSquare(pot) {
+        //     console.log('getSquare')
+        //     return Math.pow(this.counter, pot)
+        // },
+        increase() {
+            this.counter++
+        },
+        decrease() {
+            this.counter--
+        }
+    },
+    computed: {
+        squareCounter() {
+            // console.log('ComputerSquare');
+            return Math.pow(this.counter, 2);
+        }
+    }
+}
+</script>
+
+<style>
+button {
+    background-color: #64BB87;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+}
+
+button:hover {
+    background-color: darkgreen;
+    transition: 0.3s ease-in-out;
+}
+</style>
---
+++ src/components/Indecision.vue
@@ -0,0 +1,106 @@
+<template>
+    <img v-if="image" :src="image" alt="bg">
+    <div class="bg-dark"></div>
+    <div class="indecision-container">
+        <input type="text" placeholder="Hazme una pregunta" v-model="question">
+        <p>Recuerda terminar con un signo de interrojación (?)</p>
+        <div>
+            <h2>{{ question }}</h2>
+            <h1 v-show="isValidQuestion">{{ answerDictionary[answer] }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            question: '',
+            answer: 'Si, No, Pensando...',
+            image:null,
+            isValidQuestion: false,
+            answerDictionary: {
+                'yes': 'Si!',
+                'no': 'No!',
+                'maybe': 'Tal vez...'
+            }
+        }
+    },
+    methods: {
+        async getAnswer() {
+            this.answer = 'Pensando...'
+            const {answer, image} = await fetch('https://yesno.wtf/api').then(r => r.json())
+            this.answer = answer
+            this.image = image
+        }
+    },
+    watch: {
+        question(value, oldValue) {
+            this.isValidQuestion = false;
+
+            console.log(value, oldValue);            
+            if(!value.includes('?')) return
+
+            this.isValidQuestion = true; 
+            if(oldValue.includes('?')) return           
+            this.getAnswer()
+        }
+    }    
+
+}
+</script>
+
+<style scoped>
+img,
+.bg-dark {
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    z-index: 0;
+    min-height: 100%;
+    min-width: 100%;
+    background-position: center;
+    background-position-x: center;
+    background-position-y: center;
+    background-size: cover;
+    background-attachment: fixed;
+}
+
+.bg-dark {
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+.indecision-container {
+    position: relative;
+    z-index: 99;
+}
+
+input {
+    width: 250px;
+    padding: 10px 15px;
+    border-radius: 5px;
+    border: none;
+}
+
+input:focus {
+    outline: none;
+}
+
+p {
+    color: white;
+    font-size: 20px;
+    margin-top: 5px;
+}
+
+h1,
+h2 {
+    color: white;
+}
+
+h2 {
+    margin-top: 150px;
+}
+</style>
